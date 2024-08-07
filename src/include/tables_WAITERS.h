05c717e5ede8b13b5295f1d055aa6d3e55349c09
fabio <fabiof17@hotmail.fr>
2024-08-07 12:53:22 +0200

    MAJ

--- src/include/tables_WAITERS.h
+++ src/include/tables_WAITERS.h
@@ -3,19 +3,37 @@
 
 
 
-extern const u8                     TABLE_MAX_PIES[4];
+//extern const u8                     TABLE_MAX_PIES[4];
 
-//extern const Image                 *TABLE_TILES_MAN_1[9];
+
 
 
 extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MOE[59];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MOE_UP[59];
+extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MOE_DOWN[59];
+
 extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_CURLY[59];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_CURLY_UP[59];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_CURLY_DOWN[59];
+
 extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_LARRY[59];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_LARRY_UP[59];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_LARRY_DOWN[59];
+
+
 
 
 extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MAN_2[60];
+extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MAN_2_UP[60];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MAN_2_DOWN[60];
+
 extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MAN_1[60];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MAN_1_UP[60];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_MAN_1_DOWN[60];
+
 extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_WOMAN[60];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_WOMAN_UP[60];
+//extern const struct_PIE_ANIM_       TABLE_PIE_ANIM_WOMAN_DOWN[60];
 
 
 
